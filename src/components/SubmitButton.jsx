d8cf50a5609eda3390a95f2baafce4816374ab12
Aditya-Byte <adityapawar4914@gmail.com>
2025-05-30 23:05:13 +0530

    Components: Initial commit created usable UI components.

---
+++ src/components/SubmitButton.jsx
@@ -0,0 +1,9 @@
+const SubmitButton = ({buttontext}) => {
+    return (
+        <div className="w-full max-w-[500px]">
+            <button className="w-full py-4 bg-[#2E3238] text-white font-bold hover:bg-green-600 active:bg-green-700 cursor-pointer">{buttontext}</button>
+        </div>
+    )
+}
+
+export default SubmitButton
