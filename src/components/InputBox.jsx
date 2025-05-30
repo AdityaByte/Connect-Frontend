bd3d25b4af172617c07663f1cbc3c5c1b6695d51
Aditya-Byte <adityabyte@gmail.com>
2025-05-30 23:05:13 +0530

    Components: Initial commit created usable UI components.

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,19 @@
+import { FaUser } from "react-icons/fa"
+
+const InputBox = ({ placeholder, type }) => {
+    return (
+        <div className="relative w-full max-w-[500px]">
+            <input
+                type={type}
+                className="w-full px-4 py-4 border-[#2E3238] pr-10 bg-[#1F2328]"
+                placeholder={placeholder}
+            />
+            <FaUser
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none"
+                size={20}
+            />
+        </div>
+    )
+}
+
+export default InputBox
