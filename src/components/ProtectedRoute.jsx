273447af93b2eeaa3b5cbd27198267051b0df5e1
Aditya-Byte <adityabyte@gmail.com>
2025-06-03 23:29:04 +0530

    feature: implement the secure login functionality via jwt token

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react"
 import { Navigate } from "react-router-dom"
 
 const ProtectedRoute = ({ children }) => {
-    const isAuthenticated = localStorage.getItem("authToken")
+
+    console.log("Checking for the route is protected or not.")
+
+    const [isChecking, setIsChecking] = useState(true)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            console.log("Checking the auth...")
+            const token = localStorage.getItem("token")
+            const token_expiry = localStorage.getItem("tokenExpiry")
+
+            if (!token || !token_expiry) {
+                setIsAuthenticated(false)
+                setIsChecking(false)
+                return
+            }
+
+            const now = new Date();
+            const expiryDate = new Date(token_expiry)
+            if (now >= expiryDate) {
+                console.log("token expired")
+                setIsAuthenticated(false)
+                setIsChecking(false)
+                return
+            }
+
+            try {
+
+                const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/verify-token`, {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+
+                setIsAuthenticated(res.ok)
+
+            } catch (err) {
+                console.err("Token verification failed", err)
+                setIsAuthenticated(false)
+            } finally {
+                setIsChecking(false)
+            }
+        }
+
+        checkAuth()
+    }, [])
+
+    if (isChecking) {
+        return (
+            <div className="h-screen w-screen flex justify-center items-center text-2xl text-white font-bold">
+                Loading.... 
+            </div>
+        )
+    }
+
     return isAuthenticated ? children : <Navigate to="/login" />
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import home from "../assets/svg/home.svg"
 import settings from "../assets/svg/settings.svg"
 import image from "../assets/images/img-chat-app.png"
@@ -7,14 +7,35 @@ import SearchBox from "../components/SearchBox";
 import MemberCard from "../components/MemberCard";
 import MessageBoxInput from "../components/MessageBoxInput";
 import MessageTag from "../components/MessageTag";
+import { FaSignOutAlt } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+
+    const navigate = useNavigate()
+
+    const token = localStorage.getItem("token")
+
+    const decodedData = decodeToken({ token })
+    if (!decodeToken) {
+        console.log("unable to decode the token")
+        return;
+    }
+
+    const handleSignOut = (event) => {
+        event.preventDefault();
+        localStorage.clear()
+        console.log("Logged out")
+        navigate("/")
+    }
+
     return (
         <div className="h-screen w-screen flex text-white">
             <div className="h-full w-[5%] bg-[#413A3A] flex flex-col items-center">
                 <div className="realtive h-1/4 w-full flex flex-col justify-evenly items-center">
                     <img src={home} className="w-6 h-6 text-white" />
                     <img src={settings} className="w-6 h-6 text-white" />
+                    <FaSignOutAlt size={30} className="w-6 h-6 text-white" onClick={handleSignOut} />
                 </div>
                 <div className="bottom-4 w-10 h-10 rounded-full bg-white absolute bg-center bg-cover" style={{ backgroundImage: `url(${image})` }}></div>
             </div>
@@ -38,6 +59,18 @@ const Dashboard = () => {
             </div>
         </div>
     )
+}
+
+const decodeToken = ({ token }) => {
+    const base64url = token.split('.')[1]
+    const base64 = base64url.replace(/-/g, '+').replace(/_/g, '/')
+    const jsonPayload = decodeURIComponent(atob(base64)
+        .split('')
+        .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+        .join(''))
+
+    const decodedData = JSON.parse(jsonPayload);
+    return decodedData
 }
 
 export default Dashboard
